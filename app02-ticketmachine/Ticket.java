26636b80b46cddda910f7ed43ae705216f161239
martacyrn <catwolflps@gmail.com>
2020-10-19 11:58:49 +0100

    Ticket

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,19 @@
+
+/**
+ * This class will include information
+ * about prices of tickets.
+ *
+ * @author Marta Cyrn
+ * @version 0.1 19.10.2020
+ */
+public class Ticket
+{
+
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket()
+    {
+    }
+
+}
